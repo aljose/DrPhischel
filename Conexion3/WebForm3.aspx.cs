ef174ad60e73288d6209daf7d03ac04966f3300f
Irene <irefut@gmail.com>
2016-05-10 12:10:35 -0600

    Agregar un excel a la base de datos
    revisar la conexión a la base ya que como no me conecta a la base no lo
    pude probar con nuestra base

---
+++ Conexion3/WebForm3.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Conexion3
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (!FileUploadToServer.HasFile)
+            {
+                try
+                {
+                    string path = string.Concat(Server.MapPath("~/Uploaded Folder/" + FileUploadToServer.FileName));
+                    FileUploadToServer.SaveAs(path);
+
+                    // Connection String to Excel Workbook
+                    string excelConnectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 8.0", path);
+                    OleDbConnection connection = new OleDbConnection();
+                    connection.ConnectionString = excelConnectionString;
+                    OleDbCommand command = new OleDbCommand("select * from [Sheet1$]", connection);
+                    connection.Open();
+                    // Create DbDataReader to Data Worksheet
+                    DbDataReader dr = command.ExecuteReader();
+
+                    // SQL Server Connection String
+                    string sqlConnectionString = @"Data Source=MYPC;Initial Catalog=Student;User ID=sa;Password=wintellect";
+
+                    // Bulk Copy to SQL Server 
+                    SqlBulkCopy bulkInsert = new SqlBulkCopy(sqlConnectionString);
+                    bulkInsert.DestinationTableName = "Student_Record";
+                    bulkInsert.WriteToServer(dr);
+                    Label1.Text = "Ho Gaya";
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = ex.Message;
+                }
+            }
+        }
+    }
+}
